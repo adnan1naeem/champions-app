8a213dc02a4cb23582390f71acaa748b24358170
Harris1100 <hsaleem@techtiz.co>
2023-08-30 14:43:18 +0500

    app center pushed

--- App.js
+++ App.js
@@ -1,9 +1,10 @@
-import "react-native-gesture-handler";
-import React, { useEffect } from "react";
-import Navigation from "./navigation/Navigation";
-import { SafeAreaProvider } from "react-native-safe-area-context";
-import { StatusBar, LogBox } from "react-native";
-import { Colors } from "./src/Utils/Colors";
+import 'react-native-gesture-handler';
+import React, { useEffect } from 'react';
+import Navigation from './navigation/Navigation';
+import { SafeAreaProvider } from 'react-native-safe-area-context';
+import { StatusBar, LogBox } from 'react-native';
+import { Colors } from './src/Utils/Colors';
+import codePush from 'react-native-code-push';
 
 const App = () => {
   useEffect(() => {
@@ -12,13 +13,10 @@ const App = () => {
 
   return (
     <SafeAreaProvider>
-      <StatusBar
-        backgroundColor={Colors.blueBar}
-        barStyle="light-content"
-      />
+      <StatusBar backgroundColor={Colors.blueBar} barStyle="light-content" />
       <Navigation />
     </SafeAreaProvider>
   );
 };
 
-export default App;
+export default codePush(App);
