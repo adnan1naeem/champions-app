4026f0ade4a9261dacacb70456a4f56f52a91ba2
Harris1100 <hsaleem@techtiz.co>
2023-09-15 15:22:37 +0500

    /final output

--- src/Screens/Authentication/SignIn/PinCodeScreen.js
+++ src/Screens/Authentication/SignIn/PinCodeScreen.js
@@ -19,6 +19,8 @@ import CustomButton from '../../../Components/CustomButton';
 import OtpInput from '../OTP/OTP';
 import BackButton from '../../../Components/BackButton';
 import { API_BASE_URL } from '../../../../Constants';
+import { KeyboardAwareScrollView } from 'react-native-keyboard-aware-scroll-view';
+
 
 const PinCodeScreen = ({ route }) => {
   const navigation = useNavigation();
@@ -86,79 +88,92 @@ const PinCodeScreen = ({ route }) => {
     <ImageBackground
       source={require('../../../Assets/Image/background_image.png')}
       style={{ flex: 1, backgroundColor: Colors.blueBackground }}>
-      <ScrollView>
+      <KeyboardAwareScrollView
+        extraHeight={Platform.OS === 'ios' ? 90 : 130}
+        extraScrollHeight={Platform.OS === 'ios' ? 90 : 130}
+        enableOnAndroid={true}
+        enableAutomaticScroll={true}
+        showsVerticalScrollIndicator={false}
+        contentContainerStyle={{ flexGrow: 1 }}
 
-        <Image style={{
-          height: 100,
-          width: '80%',
-          alignSelf: 'center',
-          paddingTop: 5,
-          marginTop: 35,
-        }} source={require('../../../Assets/Image/login_image.png')} resizeMode="contain" />
-        <View style={{ marginHorizontal: 15 }}>
-          <BackButton navigation={navigation} />
-        </View>
-        <View style={[styles.Login_view, { marginTop: '33%' }]}>
-          <View style={{ flex: 1, alignSelf: 'center', paddingVertical: 20 }}>
-            <Text
-              style={{
-                color: Colors.text_Color,
-                fontSize: 20,
-                fontWeight: '300',
-              }}>
-              {route?.params?.cnic}
-            </Text>
+      >
+        <ScrollView
+          contentContainerStyle={{ flexGrow: 1 }}
+          keyboardShouldPersistTaps={'always'}
+        >
+
+          <Image style={{
+            height: 100,
+            width: '80%',
+            alignSelf: 'center',
+            paddingTop: 5,
+            marginTop: 35,
+          }} source={require('../../../Assets/Image/login_image.png')} resizeMode="contain" />
+          <View style={{ marginHorizontal: 15 }}>
+            <BackButton navigation={navigation} />
           </View>
-          <View style={{ width: '78%', alignSelf: 'center', marginVertical: 50 }}>
-            <View>
+          <View style={[styles.Login_view, { marginTop: '33%' }]}>
+            <View style={{ flex: 1, alignSelf: 'center', paddingVertical: 20 }}>
               <Text
                 style={{
-                  marginTop: 20,
-                  alignSelf: 'center',
-                  fontSize: 20,
                   color: Colors.text_Color,
-                  fontWeight: '200',
+                  fontSize: 20,
+                  fontWeight: '300',
                 }}>
-                Enter your 5 digit Pin
+                {route?.params?.cnic}
               </Text>
-              <OtpInput onPress={text => setVarificationCode(text)} />
             </View>
-          </View>
-
-
-          <View style={styles.otp_not_received}>
-            {isTimerRunning && timer > 0 ? (
-              <Text style={styles.otp}>
-                If you have not received PIN then please REGENERATE After {timer} seconds
-              </Text>
-            ) : (
-              <TouchableOpacity
-                style={{
-                  height: 20,
-                  width: 130,
-                  alignSelf: 'center'
-                }}
-                onPress={() => handleForgot()}>
-                <Text style={[styles.otp, { textDecorationLine: 'underline' }]}>
-                  REGENERATE PIN
+            <View style={{ width: '78%', alignSelf: 'center', marginVertical: 50 }}>
+              <View>
+                <Text
+                  style={{
+                    marginTop: 20,
+                    alignSelf: 'center',
+                    fontSize: 20,
+                    color: Colors.text_Color,
+                    fontWeight: '200',
+                  }}>
+                  Enter your 5 digit Pin
                 </Text>
-              </TouchableOpacity>
-            )}
-          </View>
+                <OtpInput onPress={text => setVarificationCode(text)} />
+              </View>
+            </View>
 
-          <CustomButton
-            onPress={() =>
-              navigation.navigate('ChangePassword', {
-                cnic: route?.params?.cnic,
-                varificationCode: varificationCode,
-              })
-            }
-            ContainerStyle={styles.proceed_button}
-            textStyle={{ color: Colors.text_Color, textAlign: 'center' }}
-            title="Proceed"
-          />
-        </View>
-      </ScrollView>
+
+            <View style={styles.otp_not_received}>
+              {isTimerRunning && timer > 0 ? (
+                <Text style={styles.otp}>
+                  If you have not received PIN then please REGENERATE After {timer} seconds
+                </Text>
+              ) : (
+                <TouchableOpacity
+                  style={{
+                    height: 20,
+                    width: 130,
+                    alignSelf: 'center'
+                  }}
+                  onPress={() => handleForgot()}>
+                  <Text style={[styles.otp, { textDecorationLine: 'underline' }]}>
+                    REGENERATE PIN
+                  </Text>
+                </TouchableOpacity>
+              )}
+            </View>
+
+            <CustomButton
+              onPress={() =>
+                navigation.navigate('ChangePassword', {
+                  cnic: route?.params?.cnic,
+                  varificationCode: varificationCode,
+                })
+              }
+              ContainerStyle={styles.proceed_button}
+              textStyle={{ color: Colors.text_Color, textAlign: 'center' }}
+              title="Proceed"
+            />
+          </View>
+        </ScrollView>
+      </KeyboardAwareScrollView>
     </ImageBackground>
   );
 };
